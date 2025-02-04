26c4039aa3e3da3bda20a5f08df7f148d3562748
Satham Ussain <satham.edu@gmail.com>
2025-02-05 00:25:34 +0530

    Store Item Design completed

--- src/components/StoreItem.tsx
+++ src/components/StoreItem.tsx
@@ -1,4 +1,4 @@
-import { Card } from "react-bootstrap";
+import { Button, Card } from "react-bootstrap";
 import { formatCurrency } from "../utilities/formatCurrency";
 type StoreItemProps = {
   id: number;
@@ -7,8 +7,9 @@ type StoreItemProps = {
   price: number;
 };
 export const StoreItem = ({ id, name, imgUrl, price }: StoreItemProps) => {
+  const quantity = 1;
   return (
-    <Card>
+    <Card className="h-100">
       <Card.Img
         variant="top"
         src={imgUrl}
@@ -20,6 +21,28 @@ export const StoreItem = ({ id, name, imgUrl, price }: StoreItemProps) => {
           <span className="fs-2">{name}</span>
           <span className="ms-2 text-muted">{formatCurrency(price)}</span>
         </Card.Title>
+        <div className="mt-auto">
+          {quantity === 0 ? (
+            <Button className="w-100">+ Add to Cart</Button>
+          ) : (
+            <div
+              className="d-flex align-items-center flex-column justify-content-center"
+              style={{ gap: ".5rem" }}
+            >
+              <div
+                className="d-flex align-items-center justify-content-center"
+                style={{ gap: ".5rem" }}
+              >
+                <Button>-</Button>
+                <div>
+                  <span className="fs3">{quantity}</span> in cart
+                </div>
+                <Button>+</Button>
+              </div>
+              <Button variant="danger">Remove</Button>
+            </div>
+          )}
+        </div>
       </Card.Body>
     </Card>
   );
